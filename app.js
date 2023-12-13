3eb0507fadf6ff0ab216ec9d9d512c6eeef61b5f
axesaibo <128742572+axesaibo@users.noreply.github.com>
2023-12-13 20:39:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,65 @@
 const express = require('express');
+const fs = require('fs').promises; // Using promises for fs functions
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
 
-const loggerMiddleware = (req, res, next) => {
-    console.log(`[${new Date().toUTCString()}] ${req.method} ${req.url}`);
-    next();
-};
+// ReadFile Endpoint
+app.get('/readFile/:filename', async (req, res) => {
+    const { filename } = req.params;
 
-
-const authenticateUser = (req, res, next) => {
-    
-};
-
-
-app.get('/dashboard', authenticateUser, (req, res) => {
-    
+    try {
+        const content = await fs.readFile(filename, 'utf-8');
+        res.status(200).send(content);
+    } catch (err) {
+        res.status(404).send('File not found');
+    }
 });
 
+// WriteFile Endpoint
+app.post('/writeFile/:filename', async (req, res) => {
+    const { filename } = req.params;
+    const { data } = req.body;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(loggerMiddleware);
+    if (!data) {
+        return res.status(400).send('No data provided');
+    }
 
+    try {
+        await fs.writeFile(filename, data, 'utf-8');
+        res.status(201).send('File written successfully');
+    } catch (err) {
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-const ecommerceRoutes = require('./routes/ecommerceRoutes');
-const passwordStrengthRoutes = require('./routes/passwordStrengthRoutes');
+// UpdateFile Endpoint
+app.put('/updateFile/:filename', async (req, res) => {
+    const { filename } = req.params;
+    const { data } = req.body;
 
+    if (!data) {
+        return res.status(400).send('No new data provided');
+    }
 
-app.use('/ecommerce', ecommerceRoutes);
-app.use('/password', passwordStrengthRoutes);
+    try {
+        await fs.appendFile(filename, `\n${data}`, 'utf-8');
+        res.status(200).send('File updated successfully');
+    } catch (err) {
+        res.status(500).send('Internal Server Error');
+    }
+});
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
